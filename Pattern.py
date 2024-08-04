11eb45fa2c5679f89174369a425bc5083a31f14e
singhnavneet77 <143984860+singhnavneet77@users.noreply.github.com>
2024-08-04 13:18:13 +0530

    new code

--- Pattern.py
+++ Pattern.py
@@ -55,3 +55,24 @@
 #    *****
 #   *******
 #  *********
+
+#<------------------------------>
+n=int(input("Number "));
+# for i in range(n):
+#     for j in range(i+1):
+#         print("* ",end=" ")
+#     print()
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print("* ",end=" ")
+#     print()
+#output
+# *  
+# *  *  
+# *  *  *  
+# *  *  *  *  
+# *  *  *  *  *  
+# *  *  *  *  
+# *  *  *  
+# *  *  
+# * 
